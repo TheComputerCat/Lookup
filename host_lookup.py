9537ebd6b028f0c7d5b97ae74c7c9760b67fd6a2
cpedraza <cpedraza@unal.edu.co>
2023-05-05 22:28:26 -0500

    added a function to read ip addresses file

--- host_lookup.py
+++ host_lookup.py
@@ -3,6 +3,12 @@
 
 import subprocess
 
+def getAddressList():
+    with open("./data/ip_list", "r") as f:
+        addresses = f.read()
+    return list(set([address for address in addresses.split("\n") if address != ""]))
+
+
 def getNmapInfoOf(address: str, isIPv6: bool):
     command = ["nmap", "-sT", "-sU", "-verbose", address]
     result = subprocess.run(command, capture_output=True, text=True)
--- test_host_lookup.py
+++ test_host_lookup.py
@@ -18,6 +18,49 @@ class Test(unittest.TestCase):
             capture_output=True,
             text=True
         )
+    
+
+    @patch("builtins.open", new_callable=mock_open, read_data="")
+    def test_read_empty_ip_list_file(self, mockFile):
+        addresses = host_lookup.getAddressList()
+
+        self.assertEqual(addresses, [])
+        mockFile.assert_called_once_with("./data/ip_list", "r")
+    
+    @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8")
+    def test_read_ip_list_file_with_one_address(self, mockFile):
+        addresses = host_lookup.getAddressList()
+
+        self.assertEqual(addresses, ["8.8.8.8"])
+        mockFile.assert_called_once_with("./data/ip_list", "r")
+    
+    @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8\n1.1.1.1")
+    def test_read_ip_list_file_with_two_addresses(self, mockFile):
+        addresses = host_lookup.getAddressList()
+
+        self.assertEqual(addresses, ["8.8.8.8", "1.1.1.1"])
+        mockFile.assert_called_once_with("./data/ip_list", "r")
+    
+    @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8\n\n1.1.1.1")
+    def test_read_ip_list_file_with_two_address_and_multiple_newlines(self, mockFile):
+        addresses = host_lookup.getAddressList()
+
+        self.assertEqual(addresses, ["8.8.8.8", "1.1.1.1"])
+        mockFile.assert_called_once_with("./data/ip_list", "r")
+    
+    @patch("builtins.open", new_callable=mock_open, read_data="\n8.8.8.8\n\n1.1.1.1\n")
+    def test_read_ip_list_file_with_more_newlines(self, mockFile):
+        addresses = host_lookup.getAddressList()
+
+        self.assertEqual(addresses, ["8.8.8.8", "1.1.1.1"])
+        mockFile.assert_called_once_with("./data/ip_list", "r")
+    
+    @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8\n8.8.8.8")
+    def test_read_ip_list_file_with_one_address_repeated(self, mockFile):
+        addresses = host_lookup.getAddressList()
+
+        self.assertEqual(addresses, ["8.8.8.8"])
+        mockFile.assert_called_once_with("./data/ip_list", "r")
 
 
 if __name__ == "__main__":
