3bfb65c7e307304906bcfa5b7477e1c76c8cd98b
Sofia Salinas <unitargetofsofigmail.com>
2023-05-06 23:10:47 -0500

    multiple page shodan support at getShodanInfo and getAllData sleep in between

--- domain_lookup.py
+++ domain_lookup.py
@@ -1,6 +1,9 @@
 import shodan
+from random import randint
+import time
+from json import loads
 
-def getDomainsFromFile():
+def getDomainsList():
     with open("./data/domain_list",'r') as domains_file:
         domain_list = domains_file.read()
         domains_file.close()
@@ -11,10 +14,25 @@ def getShodanInfoOf(domain: str):
         key = f.read()
 
     api = shodan.Shodan(key)
-    return api.dns.domain_info(domain=domain, history=False, type=None, page=1)
+    areMorePages = True
+    info = []
+    pageNumber = 1
+    while areMorePages:
+        newPage = api.dns.domain_info(domain=domain, history=False, type=None, page=pageNumber)
+        info.append(newPage)
+        areMorePages = newPage['more']
+        pageNumber+=1
+
+    return str(info)
 
 def saveDomainInfo(domainName):
-    relativePathToNewFile =f'./data/domain_raw_data/{domainName}.txt'
+    relativePathToNewFile =f'./data/domain_raw_data/{domainName}'
     with open(relativePathToNewFile, "w") as domanInfoFile:
         domanInfoFile.write(getShodanInfoOf(domainName))
         domanInfoFile.close
+
+def getAllRawData():
+    allDomains = getDomainsList()
+    for domain in allDomains:
+        saveDomainInfo(domain)
+        time.sleep(randint(5,10))
--- test_domain_lookup.py
+++ test_domain_lookup.py
@@ -2,6 +2,7 @@ import unittest
 import domain_lookup
 import shodan
 import os
+import time
 
 from unittest.mock import (
     patch,
@@ -13,7 +14,7 @@ class Test(unittest.TestCase):
     @patch("builtins.open", new_callable=mock_open, read_data="798djfhj2208FFFEEDC4")
     def test_take_shodan_api_key(self, mockFile):
         shodan.Shodan = MagicMock()
-        shodan.Shodan().dns.domain_info = MagicMock()
+        shodan.Shodan().dns.domain_info = MagicMock(return_value={'more':False, 'ip': 12345})
         
         _ = domain_lookup.getShodanInfoOf("example.com")
 
@@ -21,23 +22,42 @@ class Test(unittest.TestCase):
         shodan.Shodan.assert_called_with("798djfhj2208FFFEEDC4")
 
     @patch("builtins.open", new_callable=mock_open, read_data="798djfhj2208FFFEEDC4")
-    def test_shodan_domain_lookup(self, _):
+    def test_shodan_domain_lookup_single_page(self, _):
         shodan.Shodan = MagicMock()
-        shodan.Shodan().dns.domain_info = MagicMock()
+        shodan.Shodan().dns.domain_info = MagicMock(return_value={'more':False, 'ip': 12345})
         
-        _ = domain_lookup.getShodanInfoOf("example.com")
-        
+        infoOptained = domain_lookup.getShodanInfoOf("example.com")
+        self.assertEqual(infoOptained,str([{'more':False, 'ip': 12345}]))
+
         shodan.Shodan().dns.domain_info.assert_called_once_with(
             domain="example.com",
             history=False,
             type=None,
             page=1
         )
+
+
+    @patch("builtins.open", new_callable=mock_open, read_data="798djfhj2208FFFEEDC4")
+    def test_shodan_domain_lookup_multiple_page(self,_):
+        shodan.Shodan = MagicMock()
+        shodan.Shodan().dns.domain_info = MagicMock(side_effect=[{'more':True, 'ip': 12345},{'more':False, 'ip': 12345}])
+        
+        infoOptained = domain_lookup.getShodanInfoOf("example.com")
+        self.assertEqual(infoOptained,str([{'more':True, 'ip': 12345},{'more':False, 'ip': 12345}]))
+
+        
+        shodan.Shodan().dns.domain_info.assert_called_with(
+            domain="example.com",
+            history=False,
+            type=None,
+            page=2
+        )
+
         
     @patch("builtins.open", new_callable=mock_open, read_data="dominio1")
     def test_read_file_single(self, mockFile):
         
-        domainList = domain_lookup.getDomainsFromFile()
+        domainList = domain_lookup.getDomainsList()
 
         self.assertEqual(domainList, ["dominio1"])
         mockFile.assert_called_once_with("./data/domain_list","r")
@@ -45,29 +65,40 @@ class Test(unittest.TestCase):
     @patch("builtins.open", new_callable=mock_open, read_data="dominio1,dominio2,dominio3")
     def test_read_file_many(self, mockFile):
         
-        domainList = domain_lookup.getDomainsFromFile()
+        domainList = domain_lookup.getDomainsList()
 
         self.assertEqual(domainList, ["dominio1","dominio2","dominio3"])
         mockFile.assert_called_once_with("./data/domain_list","r")
 
     @patch("domain_lookup.getShodanInfoOf", return_value="\{ip : 123456\}")
     def test_save_domain_info(self, mockGetInfo):
-
         domainName = "dominio1"
         targetDirectory = "./data/domain_raw_data/"
-        relativePathOfNewFile = targetDirectory+domainName+".txt"
+        relativePathOfNewFile = targetDirectory+domainName
+
         domain_lookup.saveDomainInfo(domainName)
 
         numberOfFilesCreated = len(os.listdir(targetDirectory))
-
         self.assertGreater(numberOfFilesCreated,0)
 
         os.remove(relativePathOfNewFile)
     
-            
         mockGetInfo.assert_called_once_with(
             domainName
         )
 
+    @patch('time.sleep', return_value=None)
+    @patch("domain_lookup.getShodanInfoOf", return_value= 'domain.data')
+    @patch("domain_lookup.getDomainsList",return_value = ["dominio1"])
+    def test_get_all_data_single(self, mockGetDomains,mockGetInfo,sleep):
+        domain_lookup.getAllRawData()
+
+        targetDirectory = "./data/domain_raw_data/"
+        numberOfFilesCreated = len(os.listdir(targetDirectory))
+        self.assertGreater(numberOfFilesCreated,0)
+
+        mockGetDomains.assert_called_once_with()
+        mockGetInfo.assert_called_once_with("dominio1")
+        self.assertGreaterEqual(sleep.call_count,1)
 if __name__ == "__main__":
      unittest.main()
