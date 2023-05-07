ef4f9f11866404ef290a8699f3f72fdcf5e56437
Sofia Salinas <unitargetofsofigmail.com>
2023-05-07 16:37:32 -0500

    implement and tested getDomainIp, json use

--- domain_lookup.py
+++ domain_lookup.py
@@ -1,7 +1,7 @@
 import shodan
-import random
+from random import randint
 import time
-from json import loads
+import json 
 
 def getDomainsList():
     with open("./data/domain_list",'r') as domains_file:
@@ -23,16 +23,34 @@ def getShodanInfoOf(domain: str):
         areMorePages = newPage['more']
         pageNumber+=1
 
-    return str(info)
+    return json.dumps(info)
 
 def saveDomainInfo(domainName):
     relativePathToNewFile =f'./data/domain_raw_data/{domainName}'
-    with open(relativePathToNewFile, "w") as domanInfoFile:
-        domanInfoFile.write(getShodanInfoOf(domainName))
-        domanInfoFile.close()
+    with open(relativePathToNewFile, "w") as domainInfoFile:
+        domainInfoFile.write(getShodanInfoOf(domainName))
+        domainInfoFile.close
 
 def getAllRawData():
     allDomains = getDomainsList()
     for domain in allDomains:
         saveDomainInfo(domain)
-        time.sleep(random.uniform(5,10))
+        time.sleep(randint(5,10))
+
+def getDomainIp(domainName):
+    domainIp = []
+    relativePathToNewFile =f'./data/domain_raw_data/{domainName}'
+    with open(relativePathToNewFile, "r") as domainInfoFile:
+        domainInfo = json.loads(domainInfoFile.read())
+        domainInfoFile.close()
+        for page in domainInfo:
+            for dataObject in page['data']:
+                if dataObject['type'] == 'A' or dataObject['type'] == 'AAAA':
+                    domainIp.append(dataObject['value'])
+        return ','.join(domainIp)
+    
+def createIpList():
+    allDomains = getDomainsList()
+    with open('./data/ip_list', 'w') as ipList:
+        for domain in allDomains:
+            ipList.write(getDomainIp(domain))
--- test_domain_lookup.py
+++ test_domain_lookup.py
@@ -2,7 +2,7 @@ import unittest
 import domain_lookup
 import shodan
 import os
-import time
+import json
 
 from unittest.mock import (
     patch,
@@ -27,7 +27,7 @@ class Test(unittest.TestCase):
         shodan.Shodan().dns.domain_info = MagicMock(return_value={'more':False, 'ip': 12345})
         
         infoOptained = domain_lookup.getShodanInfoOf("example.com")
-        self.assertEqual(infoOptained,str([{'more':False, 'ip': 12345}]))
+        self.assertEqual(infoOptained,json.dumps([{'more':False, 'ip': 12345}]))
 
         shodan.Shodan().dns.domain_info.assert_called_once_with(
             domain="example.com",
@@ -43,7 +43,7 @@ class Test(unittest.TestCase):
         shodan.Shodan().dns.domain_info = MagicMock(side_effect=[{'more':True, 'ip': 12345},{'more':False, 'ip': 12345}])
         
         infoOptained = domain_lookup.getShodanInfoOf("example.com")
-        self.assertEqual(infoOptained,str([{'more':True, 'ip': 12345},{'more':False, 'ip': 12345}]))
+        self.assertEqual(infoOptained,json.dumps([{'more':True, 'ip': 12345},{'more':False, 'ip': 12345}]))
 
         
         shodan.Shodan().dns.domain_info.assert_called_with(
@@ -55,7 +55,7 @@ class Test(unittest.TestCase):
 
         
     @patch("builtins.open", new_callable=mock_open, read_data="dominio1")
-    def test_read_file_single(self, mockFile):
+    def test_get_domain_single(self, mockFile):
         
         domainList = domain_lookup.getDomainsList()
 
@@ -63,7 +63,7 @@ class Test(unittest.TestCase):
         mockFile.assert_called_once_with("./data/domain_list","r")
 
     @patch("builtins.open", new_callable=mock_open, read_data="dominio1,dominio2,dominio3")
-    def test_read_file_many(self, mockFile):
+    def test_get_domain_many(self, mockFile):
         
         domainList = domain_lookup.getDomainsList()
 
@@ -97,8 +97,50 @@ class Test(unittest.TestCase):
         numberOfFilesCreated = len(os.listdir(targetDirectory))
         self.assertGreater(numberOfFilesCreated,0)
 
-        mockGetDomains.assert_called_once_with()
+        mockGetDomains.assert_called_once()
         mockGetInfo.assert_called_once_with("dominio1")
-        self.assertGreaterEqual(sleep.call_count,1)
+        sleep.assert_called_once()
+
+    @patch('time.sleep', return_value=None)
+    @patch("domain_lookup.getShodanInfoOf", side_effect= ['domain1.data','domain2.data','domain3.data'])
+    @patch("domain_lookup.getDomainsList",return_value = ["dominio1","dominio2","dominio3"])
+    def test_get_all_data_many(self, mockGetDomains,mockGetInfo,sleep):
+        domain_lookup.getAllRawData()
+
+        targetDirectory = "./data/domain_raw_data/"
+        numberOfFilesCreated = len(os.listdir(targetDirectory))
+        self.assertGreater(numberOfFilesCreated,2)
+
+        mockGetDomains.assert_called_once()
+        self.assertEqual(mockGetInfo.call_count,3)
+        self.assertEqual(sleep.call_count,3)
+
+    
+    @patch("builtins.open", new_callable=mock_open, 
+        read_data=json.dumps([
+            {'more':False, 
+             'data': [
+                {
+                    "subdomain": "*.auth.corp",
+                    "type": "CNAME",
+                    "value": "uberproxy.l.google.com",
+                    "last_seen": "2021-01-26T13:04:34.018114+00:00"
+                },
+                {
+                    "subdomain": "*.cloud.sandbox",
+                    "type": "A",
+                    "value": "74.125.142.81",
+                    "last_seen": "2021-01-15T12:57:18.133727+00:00"
+                }
+                ]
+            }
+        ])
+    )
+    def test_get_domain_single_ip(self, domainInfo):
+        
+        domainIp = domain_lookup.getDomainIp('dominio1')
+
+        self.assertEqual(domainIp, "74.125.142.81")
+
 if __name__ == "__main__":
      unittest.main()
