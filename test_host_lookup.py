2ce4067775a01df36116f66a66812542fbf1537d
cpedraza <cpedraza@unal.edu.co>
2023-05-15 20:26:03 -0500

    [CJ] - replaced some code with helpers and added parameters to functions

--- test_host_lookup.py
+++ test_host_lookup.py
@@ -51,42 +51,42 @@ class Test(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open, read_data="")
     def test_read_empty_ip_list_file(self, mockFile):
-        addresses = host_lookup.getAddressList()
+        addresses = host_lookup.getAddressListFromFile('./data/ip_list')
 
         self.assertEqual(addresses, [])
         mockFile.assert_called_once_with("./data/ip_list", "r")
     
     @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8")
     def test_read_ip_list_file_with_one_address(self, mockFile):
-        addresses = host_lookup.getAddressList()
+        addresses = host_lookup.getAddressListFromFile('./data/ip_list')
 
         self.assertEqual(addresses, ["8.8.8.8"])
         mockFile.assert_called_once_with("./data/ip_list", "r")
     
     @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8\n1.1.1.1")
     def test_read_ip_list_file_with_two_addresses(self, mockFile):
-        addresses = host_lookup.getAddressList()
+        addresses = host_lookup.getAddressListFromFile('./data/ip_list')
 
         self.assertEqual(addresses, ["8.8.8.8", "1.1.1.1"])
         mockFile.assert_called_once_with("./data/ip_list", "r")
     
     @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8\n\n1.1.1.1")
     def test_read_ip_list_file_with_two_address_and_multiple_newlines(self, mockFile):
-        addresses = host_lookup.getAddressList()
+        addresses = host_lookup.getAddressListFromFile('./data/ip_list')
 
         self.assertEqual(addresses, ["8.8.8.8", "1.1.1.1"])
         mockFile.assert_called_once_with("./data/ip_list", "r")
     
     @patch("builtins.open", new_callable=mock_open, read_data="\n8.8.8.8\n\n1.1.1.1\n")
     def test_read_ip_list_file_with_more_newlines(self, mockFile):
-        addresses = host_lookup.getAddressList()
+        addresses = host_lookup.getAddressListFromFile('./data/ip_list')
 
         self.assertEqual(addresses, ["8.8.8.8", "1.1.1.1"])
         mockFile.assert_called_once_with("./data/ip_list", "r")
     
     @patch("builtins.open", new_callable=mock_open, read_data="8.8.8.8\n8.8.8.8")
     def test_read_ip_list_file_with_one_address_repeated(self, mockFile):
-        addresses = host_lookup.getAddressList()
+        addresses = host_lookup.getAddressListFromFile('./data/ip_list')
 
         self.assertEqual(addresses, ["8.8.8.8"])
         mockFile.assert_called_once_with("./data/ip_list", "r")
@@ -98,7 +98,7 @@ class saveShodanInfoOfTest(unittest.TestCase):
     def test_create_an_api_instance(self, mockFile):
         shodan.Shodan = Mock()
 
-        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "data/test_data/shodan_api_key")
+        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "data/", "data/test_data/shodan_api_key")
 
         shodan.Shodan.assert_has_calls([call("798djfhj2208FFFEEDC4")])
 
@@ -106,7 +106,7 @@ class saveShodanInfoOfTest(unittest.TestCase):
     def test_read_the_ip_list(self, mockFile):
         shodan.Shodan = Mock()
 
-        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "data/test_data/shodan_api_key")
+        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "./data/ip_raw_data/", "data/test_data/shodan_api_key")
 
         self.assertEqual(mockFile.call_count, 2)
         mockFile.assert_has_calls([
@@ -117,7 +117,7 @@ class saveShodanInfoOfTest(unittest.TestCase):
     def test_query_each_ip(self, mockFile):
         shodan.Shodan = Mock()
 
-        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "data/test_data/shodan_api_key")
+        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "./data/ip_raw_data/", "data/test_data/shodan_api_key")
 
         shodan.Shodan.assert_has_calls([
             call().host('8.8.8.8'),
@@ -128,10 +128,10 @@ class saveShodanInfoOfTest(unittest.TestCase):
     def test_open_ip_file_for_each_ip(self, mockFile):
         shodan.Shodan = Mock()
 
-        _ = host_lookup.saveShodanInfoOf("data/test_data/ip_list", "data/test_data/shodan_api_key")
+        host_lookup.saveShodanInfoOf("data/test_data/ip_list", "data/test_data/ip_raw_data/", "data/test_data/shodan_api_key")
 
         mockFile.assert_has_calls([
-            call("data/test_data/ip_raw_data/0.0.0.0", 'w'),
+            call("data/test_data/ip_raw_data/0.0.0.0", 'w'),  
         ])
         mockFile.assert_has_calls([
             call("data/test_data/ip_raw_data/8.8.8.8",'w'),
@@ -142,7 +142,7 @@ class saveShodanInfoOfTest(unittest.TestCase):
         shodan.Shodan = Mock()
 
         path = "hello/bye/"
-        _ = host_lookup.saveShodanInfoOf(path+"ip_list", "data/test_data/shodan_api_key")
+        _ = host_lookup.saveShodanInfoOf(path+"ip_list", "hello/bye/ip_raw_data/", "data/test_data/shodan_api_key")
 
         mockFile.assert_has_calls([
             call(path+"ip_raw_data/0.0.0.0", 'w'),
