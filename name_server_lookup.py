86efa601fc7084662d1c67ed53e5e75eda1159f3
soleil <mbotello@unal.edu.co>
2023-05-16 21:06:23 -0500

    nslookup on each host in a given file

--- name_server_lookup.py
+++ name_server_lookup.py
@@ -1,4 +1,31 @@
+from common import log
+from domain_lookup import getIteratorFromCSV
+import csv
 import re
+import subprocess
+
+
+def doNsLookupToListOfHosts(hostFilePath):
+    hostList = getHostListFromPath(hostFilePath)
+    for host in hostList:
+        nslookupCommandWith(host)
+
+def nslookupCommandWith(IP:str):
+    return subprocess.run(['nslookup', IP], stdout=subprocess.PIPE)
+
+def getHostListFromPath(path: str):
+    CSVIterator, f = getIteratorFromCSV(path)
+
+    try:
+        domainColumnIndex = CSVIterator.__next__().index('host')
+    except Exception as e:
+        log(e)
+        return []
+
+    domainList = [row[domainColumnIndex] for row in CSVIterator]
+    f.close()
+
+    return domainList
 
 def parseNameServerLookupOutput(output:str):
     s1 = str(re.escape('name ='))
--- test_name_server_lookup.py
+++ test_name_server_lookup.py
@@ -1,7 +1,12 @@
 import unittest
 import name_server_lookup
+import os
+from unittest.mock import (
+    patch,
+    call,
+)
 
-class rDNS(unittest.TestCase):
+class nameServerLookupTest(unittest.TestCase):
     def test_parse_nslookup_output_case1(self):
         commandReturn = '0.0.0.0.in-addr.arpa\tname =my.name.Authoritative answers can be found from:\n\n'
         domain = 'my.name.'
@@ -13,6 +18,43 @@ class rDNS(unittest.TestCase):
         domain = 'your.lastname.'
         parseResult = name_server_lookup.parseNameServerLookupOutput(commandReturn)
         self.assertEqual(parseResult, domain)
+
+    def test_get_file(self):
+        '''
+            Given a CSV file with a single column with domain names,
+            when getRowsFromCSV is called with the path of that file,
+            it should return a list with all the domains in the column.
+        '''
+        path = './data/hosts'
+        f = open(path, 'w')
+        f.write('host\n0.0.0.0\n1.1.1.1')
+        f.close()
+
+        res = name_server_lookup.getHostListFromPath(path)
+
+        self.assertEqual(res, ["0.0.0.0", "1.1.1.1"])
+
+        os.remove(path)
+    @patch("subprocess.run")
+    def test_nslookup_is_being_run_correctly_for_host(self, mockRun):
+        '''
+            Given a CSV with a single column with host directions
+            When doNsLookupToListOfIp is called with the path to that file
+            It should run nslookup command on each host
+        '''
+        path = './data/hosts'
+        f = open(path, 'w')
+        f.write('host\n0.0.0.0\n1.1.1.1')
+        f.close()
+
+        res = name_server_lookup.doNsLookupToListOfHosts(path)
+
+        os.remove(path)
+
+        mockRun.assert_has_calls([
+            call(['nslookup', '0.0.0.0'], stdout=-1),
+            call(['nslookup', '1.1.1.1'], stdout=-1),
+        ])
 
 if __name__ == '__main__':
     unittest.main()
