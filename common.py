451849dd842228f154b633ba5571fc0ea7050b70
Juan Sanchez <jusanchez@unal.edu.co>
2023-05-19 08:29:48 -0500

    Testing mode and fixture creator added

--- common.py
+++ common.py
@@ -1,7 +1,8 @@
 import datetime
+import inspect
 import os
 
-def log(message, logPath="./data/log", debug=False):
+def log(message, logPath="./data/log", debug=False, testing = True):
     if debug:
         raise message
     try:
@@ -9,7 +10,8 @@ def log(message, logPath="./data/log", debug=False):
         logMessage = "{} : {}\n\n".format(datetime.datetime.now(), message)
         f.write(logMessage)
         f.close()
-        print(logMessage)
+        if not testing:
+            print(logMessage)
     except Exception as e:
         print(e)
 
@@ -38,3 +40,23 @@ def writeStringToFile(path: str, content: str, overwrite: bool=False):
 
 def getTimeString():
     return datetime.datetime.now().strftime("-%Y:%m:%d-%H:%M:%S")
+
+def createFixture(setup, teardown):
+    def decorator(**kwargsDecorator):
+        def wrapper(function):
+            def functionToTest(*args, **kargs):
+                if setup:
+                    setup(**filterKargsForFunction(setup, kwargsDecorator))
+                try:
+                    filterKargsDecorator = filterKargsForFunction(function, kwargsDecorator)
+                    function(*args, **{**kargs, **filterKargsDecorator})
+                finally:
+                    if teardown:
+                        teardown(**filterKargsForFunction(teardown, kwargsDecorator))
+            return functionToTest
+        return wrapper
+    return decorator
+            
+def filterKargsForFunction(function, kargs):
+    functionKargs = inspect.signature(function).parameters
+    return { karg: kargs[karg] for karg in kargs if karg in functionKargs }
