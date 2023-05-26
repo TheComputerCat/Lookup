3ce932d462df67cf474df590aab2e5cbc446ac16
Sofia Salinas <unitargetofsofigmail.com>
2023-05-26 16:27:37 -0500

    refactor of validation

--- DB_DTO.py
+++ DB_DTO.py
@@ -1,22 +1,28 @@
 from marshmallow import Schema, fields, EXCLUDE, ValidationError
 from common import log
-class ongs(Schema):
+class ORGS(Schema):
     id = fields.Integer(allow_none=True)
-    dominio = fields.Str(required = True, error_messages={'required': 'An ong needs at least a name'})
-    pais = fields.Str(allow_none=True)
+    domain = fields.String(required = True)
     class Meta:
         unknown = EXCLUDE
 
-def validate_with_DTO(table_name,object):
+class A_RECORDS(Schema):
+    id = fields.Integer(allow_none=True)
+
+
+class DOMAINS(Schema):
+    id = fields.Integer(allow_none=True)
+
+def validate_with_DTO(table_name,dict):
     try:
         validator_schema = get_correct_DTO(table_name)
-        validator_schema.validate(object)
-    except ValidationError as e:
+        validator_schema.load(dict)
+        return True
+    except (ValidationError, Exception) as e:
         log(e)
         return False
-    return True
 def get_correct_DTO(table_name):
     schemas = {
-        'ongs' : ongs()
+        'ORGS' : ORGS()
     }
     return schemas[table_name]
--- test_DB.py
+++ test_DB.py
@@ -14,26 +14,29 @@ withATextFile = createFixture(setUpWithATextFile, tearDownWithATextFile)
 
 class TestValidateWithDTO(unittest.TestCase):
     def test_validate_true(self):
-        isvalid = DB_DTO.validate_with_DTO('ongs',{
-            'dominio': "pepe",
-            'pais': 'colombia'})
+        isvalid = DB_DTO.validate_with_DTO('ORGS',{
+            'id':2,
+            'domain': "pepe",})
         self.assertTrue(isvalid)
 
     def test_validate_allowing_missing_values(self):
-        isvalid = DB_DTO.validate_with_DTO('ongs',{
-            'dominio': "pepe"})
+        isvalid = DB_DTO.validate_with_DTO('ORGS',{
+            'domain': "pepe"})
         self.assertTrue(isvalid)
 
     def test_validate_failing_missing_required_values(self):
-        isvalid = DB_DTO.validate_with_DTO('ongs',{
-            'pais': "colombia"})
+        dict_to_validate = {'id': 27}
+        isvalid = DB_DTO.validate_with_DTO('ORGS',dict_to_validate)
         self.assertRaises(ValidationError)
+        self.assertFalse(isvalid)
     
     def test_validate_failing_wrong_type(self):
-        isvalid = DB_DTO.validate_with_DTO('ongs',{
-            'dominio': 1234 ,
-            'pais': "colombia"})
+        dict_to_validate = {
+            'domain': 1234, }
+        isvalid = DB_DTO.validate_with_DTO('ORGS',dict_to_validate)
         self.assertRaises(ValidationError)
+        self.assertFalse(isvalid)
+    
 
 
 if __name__ == '__main__':
