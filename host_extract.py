6bf669f24b8db034cc00a23a34d6bd3c57891c32
cpedraza <cpedraza@unal.edu.co>
2023-05-28 11:40:31 -0500

    deleted db helpers from host_extract

--- host_extract.py
+++ host_extract.py
@@ -2,8 +2,6 @@ from common import (
     tryTo,
 )
 
-import DB_API
-
 def getAttrFromDict(dict, key):
     return tryTo(lambda: dict[key], None)
 
@@ -47,62 +45,3 @@ def getServiceRowFromServiceDict(serviceDict):
         "name": getAttrFromDict(serviceDict, "service"),
         "version": getAttrFromDict(serviceDict, "version"),
     }
-
-def insertServiceRowsFromTrimmedDict(trimmedDict):
-    SERVICES = "SERVICES"
-
-    rowsToInsert = filter(
-        lambda row: not DB_API.isRowInTable(SERVICES, row),
-        map(getServiceRowFromServiceDict, trimmedDict["services"])
-    )
-
-    for row in rowsToInsert:
-        DB_API.insert_in(SERVICES, row)
-
-def getHostRowID(address):
-    HOSTS = "HOSTS"
-
-    row = {"address": address}
-    if not DB_API.isRowInTable(HOSTS, row):
-        DB_API.insert_in(HOSTS, row)
-    
-    query = DB_API.searchForSingleRowQuery(HOSTS, row, cols=["id"])
-
-    response = DB_API._execute(query, return_results=True)
-
-    return tryTo(lambda: response[0][0], None)
-
-def getServiceRowID(serviceDict):
-    SERVICES = "SERVICES"
-
-    row = getServiceRowFromServiceDict(serviceDict)
-    if not DB_API.isRowInTable(SERVICES, row):
-        DB_API.insert_in(SERVICES, row)
-    
-    query = DB_API.searchForSingleRowQuery(SERVICES, row, cols=["id"])
-
-    response = DB_API._execute(query, return_results=True)
-
-    return tryTo(lambda: response[0][0], None)
-
-def getHostServiceRowFromAddressID(addressRowID):
-    return lambda serviceDict: {
-        "host": addressRowID,
-        "service": getServiceRowID(serviceDict),
-        "timestamp": serviceDict["timestamp"],
-        "port": serviceDict["port"],
-    }
-
-def insertHostServiceRowsFromTrimmedDict(trimmedDict):
-    HOST_SERVICES = "HOST_SERVICES"
-
-    addressRowID = getHostRowID(trimmedDict["address"])
-    addressFixer = getHostServiceRowFromAddressID(addressRowID)
-
-    rowsToInsert = filter(
-        lambda row: not DB_API.isRowInTable(HOST_SERVICES, row),
-        map(addressFixer, trimmedDict["services"])
-    )
-
-    for row in rowsToInsert:
-        DB_API.insert_in(HOST_SERVICES, row)
