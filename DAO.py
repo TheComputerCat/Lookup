12905912e4eb364e2a01b3b48a0a0b05e0b19100
cpedraza <cpedraza@unal.edu.co>
2023-05-28 09:15:04 -0500

    reformatted the tables with the ORM

--- DAO.py
+++ DAO.py
@@ -1,3 +1,7 @@
+from datetime import (
+    datetime,
+)
+
 from sqlalchemy import (
     create_engine,
     Column,
@@ -9,38 +13,57 @@ from sqlalchemy import (
     Table,
 )
 
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    mapped_column,
+    Mapped,
+    relationship,
+)
+
+from typing import (
+    List,
+    Optional,
+)
+
 metadata_obj = MetaData()
+class Base(DeclarativeBase):
+    pass
 
-hosts_table = Table(
-    "HOSTS",
-    metadata_obj,
-    Column("id", Integer, primary_key=True),
-    Column("address", String(15), nullable=False)
-)
+class Host(Base):
+    __tablename__ = "HOSTS"
 
-services_table = Table(
-    "SERVICES",
-    metadata_obj,
-    Column("id", Integer, primary_key=True),
-    Column("name", String(100), nullable=True),
-    Column("version", String(100), nullable=True)
-)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    address: Mapped[str] = mapped_column(String(15))
 
-cpe_codes_table = Table(
-    "CPE_CODES",
-    metadata_obj,
-    Column("id", Integer, primary_key=True),
-    Column("code", String(100), nullable=False),
-    Column("service", ForeignKey("SERVICES.id"), nullable=False)
-)
+    host_services: Mapped[List["HostService"]] = relationship(back_populates="host")
 
-host_services_table = Table(
-    "HOST_SERVICES",
-    metadata_obj,
-    Column("id", Integer, primary_key=True),
-    Column("host", ForeignKey("HOSTS.id"), nullable=False),
-    Column("service", ForeignKey("SERVICES.id"), nullable=True),
-    Column("timestamp", DateTime, nullable=False),
-    Column("port", Integer, nullable=False),
-    Column("source", String(30), nullable=False)
-)
+class Service(Base):
+    __tablename__ = "SERVICES"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    version: Mapped[str] = mapped_column(String(100), nullable=True)
+
+    cpe_code: Mapped["CPECode"] = relationship(back_populates="service")
+    host_services: Mapped[List["HostService"]] = relationship(back_populates="service")
+
+class HostService(Base):
+    __tablename__ = "HOST_SERVICES"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    host_id: Mapped[int] = mapped_column(ForeignKey("HOSTS.id"), nullable=False)
+    service_id: Mapped[int] = mapped_column(ForeignKey("SERVICES.id"), nullable=True)
+    source: Mapped[str] = mapped_column(String(100), nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    host: Mapped[Host] = relationship(back_populates="host_services")
+    service: Mapped[Service] = relationship(back_populates="host_services")
+
+class CPECode(Base):
+    __tablename__ = "CPE_CODES"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    code: Mapped[str] = mapped_column(String(100), nullable=False)
+    service_id: Mapped[int] = mapped_column(ForeignKey("SERVICES.id"), nullable=False)
+
+    service: Mapped[Service] = relationship(back_populates="cpe_code")
