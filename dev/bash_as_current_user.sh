7bb172a280275a18cdd3619713473298b94ad818
Juan Sanchez <jusanchez@unal.edu.co>
2023-06-26 21:33:01 -0500

    [J] now docker user can execute commands as sudo

--- dev/bash_as_current_user.sh
+++ dev/bash_as_current_user.sh
@@ -12,5 +12,6 @@ addgroup -g $CURR_USER_GID grp
 adduser -D -h /home/user -u $CURR_USER_UID -G grp user
 addgroup user docker
 echo "cd $VOLUME" >> /home/user/.profile
+echo "user  ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers.d/username
 
 su - user
